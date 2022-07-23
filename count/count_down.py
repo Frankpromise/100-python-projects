e59e72cb709f1d7465d00b09b8e8eea08517ddfb
Frank Promise Edah <99150197+Frankpromise@users.noreply.github.com>
2022-07-23 11:31:13 +0200

    Update count_down.py

--- count/count_down.py
+++ count/count_down.py
@@ -3,7 +3,7 @@ import time
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
-        timer = "{:02d}:{:02}" .format(mins, secs)
+        timer = "{:02d}:{:02d}" .format(mins, secs)
         print(timer)
         time.sleep = (0)
         t-=1
